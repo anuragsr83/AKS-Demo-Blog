dadd74694564b73a0a8ee6fc6e88b5d9cc4759ea
Anurag <anurag_vani83outlook.com>
2022-12-27 13:30:32 +0530

    Initial commit

---
+++ OrderAPIProject/OrderAPIProject/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrderAPIProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>()
+            {
+             new Order { OrderId=1, ProductName="Product 1", ProductId=1,ProductPrice=100,OrderQty=10,CreatedDate=DateTime.Now },
+             new Order { OrderId=2, ProductName="Product 1", ProductId=1,ProductPrice=100,OrderQty=10,CreatedDate=DateTime.Now },
+             new Order { OrderId=3, ProductName="Product 3", ProductId=3,ProductPrice=300,OrderQty=30,CreatedDate=DateTime.Now },
+             new Order { OrderId=4, ProductName="Product 2", ProductId=2,ProductPrice=200,OrderQty=20,CreatedDate=DateTime.Now },
+             new Order { OrderId=5, ProductName="Product 5", ProductId=5,ProductPrice=500,OrderQty=50,CreatedDate=DateTime.Now }            
+            };
+        }
+
+    }
+}
---
+++ ProductAPIProject/ProductAPIProject/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {        
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+            new Product {ProductId=1, Name="Product 1",Price=100,CreatedDate=DateTime.Now },
+            new Product {ProductId=2, Name="Product 2",Price=200,CreatedDate=DateTime.Now },
+            new Product {ProductId=3, Name="Product 3",Price=300,CreatedDate=DateTime.Now },
+            new Product {ProductId=4, Name="Product 4",Price=400,CreatedDate=DateTime.Now },
+            new Product {ProductId=5, Name="Product 5",Price=500,CreatedDate=DateTime.Now },
+            };
+        }
+    }
+}
